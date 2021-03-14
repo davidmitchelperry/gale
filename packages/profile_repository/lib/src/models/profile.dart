892cbc4e464ba1aa3abb22c9806e21a2bdb279d9
David Perry <davidmitchelperry@gmail.com>
2021-03-14 14:50:27 -0400

    Revert "Added imageUrls to profile model in the profile repo"
    This reverts commit 44c8cc1588be9547c8721b9bba1519b0a7fae6ca.

--- packages/profile_repository/lib/src/models/profile.dart
+++ packages/profile_repository/lib/src/models/profile.dart
@@ -6,26 +6,20 @@ class Profile {
   final String firstName;
   final String lastName;
   final String profilePicUrl;
-  final List<String> imageUrls;
 
-  Profile(this.firstName, this.lastName, this.profilePicUrl, this.imageUrls);
+  Profile(this.firstName, this.lastName, this.profilePicUrl);
 
-  Profile copyWith({
-    String firstName,
-    String lastName,
-    String profilePicUrl,
-    List<String> imageUrl,
-  }) {
-    return Profile(firstName ?? this.firstName, lastName ?? this.lastName,
-        profilePicUrl ?? this.profilePicUrl, imageUrls ?? this.imageUrls);
+  Profile copyWith({String firstName, String lastName, String profilePicUrl}) {
+    return Profile(
+      firstName ?? this.firstName,
+      lastName ?? this.lastName,
+      profilePicUrl ?? this.profilePicUrl,
+    );
   }
 
   @override
   int get hashCode =>
-      firstName.hashCode ^
-      lastName.hashCode ^
-      profilePicUrl.hashCode ^
-      imageUrls.hashCode;
+      firstName.hashCode ^ lastName.hashCode ^ profilePicUrl.hashCode;
 
   @override
   bool operator ==(Object other) =>
@@ -34,12 +28,11 @@ class Profile {
           runtimeType == other.runtimeType &&
           firstName == other.firstName &&
           lastName == other.lastName &&
-          profilePicUrl == other.profilePicUrl &&
-          imageUrls == other.imageUrls;
+          profilePicUrl == other.profilePicUrl;
 
   @override
   String toString() {
-    return 'Profile{firstName: $firstName, lastName: $lastName, profilePicUrl: $profilePicUrl, imageUrls: $imageUrls }';
+    return 'Profile{firstName: $firstName, lastName: $lastName, profilePicUrl: $profilePicUrl}';
   }
 
   ProfileEntity toEntity() {
@@ -51,7 +44,6 @@ class Profile {
       entity.firstName,
       entity.lastName,
       entity.profilePicUrl,
-      [],
     );
   }
 }
